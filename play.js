c88182c05fce5ab276e52283266601d413fa3ca9
pdporterman <pdporter@byu.edu>
2023-10-23 10:53:57 -0600

    more JS

--- play.js
+++ play.js
@@ -18,7 +18,10 @@ function read_input(){
 
 function move(){ // func 1
     input = read_input()
-    if (current == input - 1){
+    if (input < 1 || input > 6){
+        document.getElementById("text_box").innerHTML +="<br>"+ ("not a room")
+    }
+    else if (current == input - 1){
         document.getElementById("text_box").innerHTML +="<br>"+ ("You stay put")
     }
     else{
@@ -26,11 +29,32 @@ function move(){ // func 1
         current = input-1
     }
     func = 0
-    document.getElementById("moves").textContent = "1) Move 2) Question 3) Accuse"
+    if (current == 5){
+        document.getElementById("moves").textContent = "1) Move 2) Question 3) Accuse"
+    }
+    else {
+        document.getElementById("moves").textContent = "1) Move 2) Search 3) Accuse"
+    }
 }
 
 function question(){
-
+    input = read_input()
+    if (input < 1 || input > 4){
+        document.getElementById("text_box").innerHTML +="<br>"+ ("not a person")
+    }
+    else if (input == 1){
+        document.getElementById("text_box").innerHTML +="<br>"+ ("You talk to Cambell the chef, he tells you he came from the kitchen")
+    }
+    else if (input == 2){
+        document.getElementById("text_box").innerHTML +="<br>"+ ("You talk to Johnny the mechanic,he tells you he came from the garage")
+    }
+    else if (input == 3){
+        document.getElementById("text_box").innerHTML +="<br>"+ ("You talk to Sara the pianist, she tells you he came from the ballroom")
+    }
+    else if (input == 4){
+        document.getElementById("text_box").innerHTML +="<br>"+ ("You talk to Kim the author, she tells you he came from the library")
+    }
+    func = 0
 }
 
 function search(){
@@ -67,8 +91,11 @@ function get_action() {//func0
             func = 1
         }
         else if (action == 2){
-            if (current == 6)
-                question()
+            if (current == 6){
+            document.getElementById("text_box").innerHTML +="<br>"+ ("who do you want to question?")
+            document.getElementById("moves").textContent = "1) Cambell 2) Johnny 3) Sara 4) Kim"
+            func = 2
+            }
             else {
                 search()
             }
@@ -84,7 +111,7 @@ function get_action() {//func0
 function set_var(ID) {
     document.getElementById(ID).innerHTML +="<br>"+("oh no there is a dead guy");
     if (select === 1) {
-        suspect = "Cambel"
+        suspect = "Cambell"
         weapon = "knife"
         document.getElementById(ID).innerHTML +="<br>"+(select);
     } else if (select === 2) {
@@ -93,11 +120,11 @@ function set_var(ID) {
         document.getElementById(ID).innerHTML +="<br>"+(select);
     } else if (select === 3) {
         suspect = "Sara"
-        weapon = "candel stick"
+        weapon = "piano wire"
         document.getElementById(ID).innerHTML +="<br>"+(select);
     } else if (select === 4) {
         suspect = "Kim"
-        weapon = "piano wire"
+        weapon = "candel stick"
         document.getElementById(ID).innerHTML +="<br>"+(select);
     } 
 }
