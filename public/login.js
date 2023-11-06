80d4227cb780c5c3ffdad812f667786ea7473a33
pdporterman <pdporter@byu.edu>
2023-11-06 15:37:07 -0700

    i did the thing

---
+++ public/login.js
@@ -0,0 +1,13 @@
+const MAX_MOVES = 100000;
+document.querySelector('#submitLogin').addEventListener('click', function(e) {
+    e.preventDefault();
+
+
+    const inputUsername = document.querySelector("#name").value;
+    const inputPassword = document.querySelector("#password").value;
+
+    const user = { name: inputUsername, pass: inputPassword, moves: MAX_MOVES };
+
+    localStorage.setItem('user', JSON.stringify(user));
+    window.location.href = "main.html";
+});
